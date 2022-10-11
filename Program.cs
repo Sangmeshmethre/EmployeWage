65313addcb4ba4c31e9d860b50db0a042fb16566
Sangmeshmethre <ssmcmethre3080@gmail.com>
2022-10-12 01:12:16 +0530

    Employe9

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram195EmployeUC9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Relience", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.ToString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.ToString());
+        }
+    }
+}
