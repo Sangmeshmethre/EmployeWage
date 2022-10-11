a9243b53d9653d4ef022e05763066d35dc293c82
Sangmeshmethre <ssmcmethre3080@gmail.com>
2022-10-12 00:23:31 +0530

    EmployeWageUC1

---
+++ EmployeWage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram195Empoly
+{
+    public class EmployeWage
+    {
+        public static void EmpoyeStatus()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next();
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employe is present");
+            }
+            else
+            {
+                Console.WriteLine("Employe is absent");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram195Empoly
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeWage.EmpoyeStatus();
+        }
+    }
+}
