e272bb63becc5eabcf780dec662a3fe20d45da49
Sangmeshmethre <ssmcmethre3080@gmail.com>
2022-10-12 01:05:44 +0530

    EmployeWage8

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram195EmployeUC8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmpoyeWageUC8.computrEmpWage("DMart", 20, 2, 10);
+            EmpoyeWageUC8.computrEmpWage("Reliance", 10, 4, 20);
+        }
+    }
+
+}
