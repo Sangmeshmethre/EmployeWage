853f517b965f6810c1626e3811b1186965040832
Sangmeshmethre <ssmcmethre3080@gmail.com>
2022-10-12 00:43:06 +0530

    EmployeWage

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram195UC4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeWageUC4.EmployeWage();
+        }
+    }
+}
