ee0105f9d1dc5ab17db9daa14224d96af343bdb7
Sangmesh Methre <SangmeshMetre>
2022-10-30 17:38:58 +0530

    EmployeWage

---
+++ BasicProgarm195UC11/IComputeEmpWage.cs
@@ -0,0 +1,8 @@
+﻿namespace BasicProgarm195UC11
+{
+    public interface IComputeEmpWage
+    {
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+        public void computeEmpWage();
+    }
+}
